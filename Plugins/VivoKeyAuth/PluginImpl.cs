d6fe90c3400db119a0d2d2a8201fb4fe4eae3b75
rileyg98 <rileyg98@users.noreply.github.com>
2020-06-07 22:23:22 +1000

    Implemented basic auth plugin
    literally just always yes

---
+++ Plugins/VivoKeyAuth/PluginImpl.cs
@@ -0,0 +1,42 @@
+﻿using pGina.Shared.Types;
+using System;
+
+namespace pGina.Plugin.VivoKeyAuth
+{
+    public class PluginImpl : pGina.Shared.Interfaces.IPluginAuthentication
+    {
+        public string Name
+        {
+            get { return "VivoKeyAuth"; }
+        }
+        public string Description
+        {
+            get { return "Authenticates users with a VivoKey Apex cryptobionic implant."; }
+        }
+        private static readonly Guid m_uuid = new Guid("BE7A75A9-4734-45EF-AFCE-C188248D246E");
+
+        public Guid Uuid
+        {
+            get { return m_uuid; }
+        }
+
+        public string Version
+        {
+            get
+            {
+                return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public void Starting() { }
+
+        public void Stopping() { }
+
+
+        public BooleanResult AuthenticateUser(SessionProperties properties)
+        {
+            return new BooleanResult() { Success = true };
+        }
+
+    }
+}
